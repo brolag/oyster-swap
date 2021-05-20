5b0173dd6f397ed46323edbc8f0fcf611a720bfa
Alfredo Bonilla <brolag@live.com>
2021-05-19 23:06:02 -0600

    Updates

---
+++ precache-manifest.3365d2b2176d1b030ab04dc33ba38e79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585a8c18d0ab171ee264f31d8d2a2546",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6325d7ec048627e42fae",
+    "url": "./static/css/2.4b60f7c3.chunk.css"
+  },
+  {
+    "revision": "4f4355b13fd3e56a8bf8",
+    "url": "./static/css/main.23d0f254.chunk.css"
+  },
+  {
+    "revision": "6325d7ec048627e42fae",
+    "url": "./static/js/2.304c374f.chunk.js"
+  },
+  {
+    "revision": "1a29f8fb7f6a758d9dce8a0780f3b302",
+    "url": "./static/js/2.304c374f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4355b13fd3e56a8bf8",
+    "url": "./static/js/main.1dc0d5e6.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
